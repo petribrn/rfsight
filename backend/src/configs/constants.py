aa2d3a69d82f3d32a80f8444352bbf1b7309272c
Bruno Petri <brunoopetri@gmail.com>
2025-08-05 23:03:18 -0300

    fix: use env variables, correct color for template, updated packages

--- backend/src/configs/constants.py
+++ backend/src/configs/constants.py
@@ -6,8 +6,8 @@ from fastapi_mail import ConnectionConfig
 
 load_dotenv()
 
-API_HOST = '192.168.7.110'
-API_PORT = int(os.getenv('API_PORT', 6792))
+API_HOST = os.getenv('API_HOST')
+API_PORT = int(os.getenv('API_PORT'))
 
 SECRET_KEY = os.getenv('SECRET_KEY') # openssl rand -hex 32
 ALGORITHM = os.getenv('ALGORITHM')
@@ -18,13 +18,15 @@ RESET_PASSWORD_EXPIRE_MINUTES = 10
 SSL_CERT_PATH = './certs/cert.pem'
 SSL_KEY_PATH = './certs/key.pem'
 
+EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
+
 API_MAIL_CONF = ConnectionConfig(
-    MAIL_USERNAME='teamtechversewpro@gmail.com',
-    MAIL_PASSWORD='Lockinet@1-techrow13qwqw',
-    MAIL_FROM= 'teamtechversewpro@gmail.com',
-    MAIL_FROM_NAME= 'teamtechversewpro@gmail.com',
-    MAIL_PORT=25,
-    MAIL_SERVER='localhost',
+    MAIL_USERNAME=EMAIL_ADDRESS,
+    MAIL_PASSWORD=os.getenv('EMAIL_PASSWORD'),
+    MAIL_FROM= EMAIL_ADDRESS,
+    MAIL_FROM_NAME= EMAIL_ADDRESS,
+    MAIL_SERVER=os.getenv('EMAIL_SERVER'),
+    MAIL_PORT=os.getenv('EMAIL_PORT'),
     MAIL_STARTTLS=False,
     MAIL_SSL_TLS=False,
     USE_CREDENTIALS=True,
@@ -40,13 +42,13 @@ MONGODB_LOGIN = os.getenv('MONGODB_LOGIN')
 MONGODB_PASSWD = os.getenv('MONGODB_PASSWD')
 MONGODB_PORT = os.getenv('MONGODB_PORT')
 MONGODB_DATABASE = os.getenv('MONGODB_DATABASE')
-MONGODB_URI = f'mongodb://{MONGODB_LOGIN}:{MONGODB_PASSWD}@{MONGODB_HOST}:{MONGODB_PORT}/{MONGODB_DATABASE}?directConnection=true&authSOurce=admin'
+MONGODB_URI = f'mongodb://{MONGODB_LOGIN}:{MONGODB_PASSWD}@{MONGODB_HOST}:{MONGODB_PORT}/{MONGODB_DATABASE}?directConnection=true&?authSource={MONGODB_DATABASE}'
 
 VALIDATE_MAC_REGEX = '^([0-9A-Fa-f]{2}[:-]?){5}([0-9A-Fa-f]{2})|([0-9a-fA-F]{4}.[0-9a-fA-F]{4}.[0-9a-fA-F]{4})$'
 
 VALIDATE_PASSWD_FORMAT_REGEX = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^*-]).{8,}$"
 
-DOCKER_CONTAINER_API_INTERFACE = 'enp44s0'
+DOCKER_CONTAINER_API_INTERFACE = os.getenv('API_CONTAINER_INTERFACE')
 FIND_DEVICES_COMMAND = f"""arp-scan -I {DOCKER_CONTAINER_API_INTERFACE} --localnet -q"""
 DEVICE_CONN_TIMEOUT = 20
 
